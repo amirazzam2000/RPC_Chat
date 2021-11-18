e6118308b1109b1cb99af67a470457cb8544a55d
amirazzam2000 <ameerazzam123@gmail.com>
2021-11-18 19:57:58 +0100

    m

--- rpc_client.c
+++ rpc_client.c
@@ -36,7 +36,7 @@ program_write_1(char *host)
 		fflush(stdin);
 
 		msg.message[strlen(msg.message)] = 0;
-		print("|%s|", msg.message);
+		printf("|%s|", msg.message);
 
 		if (strcmp(msg.message, "quit") == 0) break;
 		
