5af9ab022a987180cf204f363e4016a2c0e19f03
amirazzam2000 <ameerazzam123@gmail.com>
2021-11-19 18:23:59 +0100

    m

--- rpc_client.c
+++ rpc_client.c
@@ -30,6 +30,9 @@ program_write_1(char *host)
 	fgets(msg.name, 10, stdin);
 	msg.name[ strlen(msg.name) - 1 ] = 0;
 
+	int flags = fcntl(0, F_GETFL, 0);
+	fcntl(0, F_SETFL, flags | O_NONBLOCK);
+
 	while(1)
 	{
 		//Get chat
