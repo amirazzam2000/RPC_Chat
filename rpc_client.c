a65b5de84e931898cff9b84a1be288ca3431f391
amirazzam2000 <ameerazzam123@gmail.com>
2021-11-27 19:11:36 +0100

    m

--- rpc_client.c
+++ rpc_client.c
@@ -96,7 +96,7 @@ void  writeMessage()
 	ch = mvwgetch(bottom, 2, 2 + n_chars);
 	if (ch != ERR){
 		msg.message[n_chars++] = ch;
-		{	
+			
 		if (ch == 8 || ch == '^'){
 			mvwprintw(bottom, 2, 2 + (--n_chars), "\0");
 			msg.message[n_chars] = 0;
